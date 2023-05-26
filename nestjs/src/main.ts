4c8660345f4072902d70dfde91d51544bd229c65
Dipadana Putu <dipadanadev@gmail.com>
2023-05-27 02:24:13 +0800

    Open cors

--- nestjs/src/main.ts
+++ nestjs/src/main.ts
@@ -13,6 +13,8 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  app.enableCors();
+
   const prismaService = app.get(PrismaService);
   await prismaService.enableShutdownHooks(app);
 
